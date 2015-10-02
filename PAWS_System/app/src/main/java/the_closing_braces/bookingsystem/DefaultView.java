d21968efb51ef8aeedbf2f75188e98d6125bb9c9
GodfreyMathe13103394 <u13103394@tuks.co.za>
2015-10-02 20:35:05 +0200

    Added threads and extra functionality
    Activity Status and Booking functionality added

--- PAWS_System/app/src/main/java/the_closing_braces/bookingsystem/DefaultView.java
+++ PAWS_System/app/src/main/java/the_closing_braces/bookingsystem/DefaultView.java
@@ -16,18 +16,18 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
-
+import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.NumberPicker;
 import android.widget.SimpleAdapter;
-
+import android.widget.TextClock;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
@@ -38,12 +38,16 @@ import java.text.SimpleDateFormat;
 
 import java.util.ArrayList;
 import java.util.Calendar;
-
+import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Collections;
 
+import static the_closing_braces.bookingsystem.R.drawable.status_border_green;
+import static the_closing_braces.bookingsystem.R.drawable.status_border_orange;
+import static the_closing_braces.bookingsystem.R.drawable.status_border_red;
 
 public class DefaultView extends Activity {
     String currentActivityEndTime = ""; //Used for status change
@@ -82,6 +86,129 @@ public class DefaultView extends Activity {
         todaysActivities = (ListView) findViewById(R.id.lstActivities);
         populateActivities = new SimpleAdapter(getApplicationContext(), activities, R.layout.list_view_activities_layout, new String[]{"activitySubject", "activityOrganizer", "activityStartTime", "activityEndTime"}, new int[]{R.id.subject, R.id.Organizer, R.id.startTime, R.id.endTime});
         todaysActivities.setAdapter(populateActivities);
+
+        //Thread that runs every second to test for room status
+        statusThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    while (!isInterrupted()) {
+                        Thread.sleep(500);//half a second
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Calendar calendar = Calendar.getInstance();
+                                SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
+                                String currentTime = timeFormat.format(calendar.getTime());
+
+                                //check if there is a current activity and whether there is a item in the list selected if so set focus on link list
+                                if (currentActivityEndTime != "" && todaysActivities.getSelectedItemId() != -1) {
+                                    todaysActivities.requestFocusFromTouch();
+                                    todaysActivities.requestFocus();
+                                }
+
+                                //Initialize all the activity components
+                                final TextView labelStatus = (TextView) findViewById(R.id.lblStatus);
+                                final TextView labelMinutes = (TextView) findViewById(R.id.minutesTill);
+                                final TextClock clockTime = (TextClock) findViewById(R.id.clockTime);
+                                final TextClock clockDate = (TextClock) findViewById(R.id.clockDate);
+                                ListView todaysActivities = (ListView) findViewById(R.id.lstActivities);
+
+                                //Time format to set the correct data
+                                SimpleDateFormat timeFormat2 = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+                                String date = timeFormat2.format(calendar.getTime());
+
+                                //For loop runs through all the activities
+                                for (int i = 0; i < todaysActivities.getAdapter().getCount(); i++) {
+                                    //retrieve current activity
+                                    Map<String, String> activity = (Map<String, String>) todaysActivities.getAdapter().getItem(i);
+                                    try {
+                                        //retrieve info on current activity
+                                        Date startTime = timeFormat2.parse(date.substring(0, 10) + " " + activity.get("activityStartTime"));
+                                        Date endTime = timeFormat2.parse(date.substring(0, 10) + " " + activity.get("activityEndTime"));
+
+                                        //if the current time is in betwen the activities start and end times it means the activity is currently ongoing
+                                        if (calendar.getTime().after(startTime) && calendar.getTime().before(endTime)) {
+                                            //select it in the List View
+                                            todaysActivities.setSelection(i);
+                                            todaysActivities.requestFocusFromTouch();
+                                            todaysActivities.requestFocus();
+
+                                            //Change status color to red and make text white for easier visibility
+                                            FrameLayout statusBlock = (FrameLayout) findViewById(R.id.statusBlock);
+                                            statusBlock.setBackgroundResource(status_border_red);
+                                            labelStatus.setTextColor(Color.parseColor("#ffaaaaaa"));
+                                            clockTime.setTextColor(Color.parseColor("#ffaaaaaa"));
+                                            clockDate.setTextColor(Color.parseColor("#ffaaaaaa"));
+
+                                            //Change status to In Use
+                                            labelMinutes.setVisibility(View.INVISIBLE);
+                                            labelStatus.setText("IN USE");
+
+                                            //Set the activity end time
+                                            currentActivityEndTime = timeFormat.format(endTime);
+
+                                        }
+                                    } catch (ParseException e) {
+                                    }
+
+                                }
+
+                                //For loop runs through all the activities
+                                for (int i = 0; i < todaysActivities.getAdapter().getCount(); i++) {
+                                    //retrieve current activity
+                                    Map<String, String> activity = (Map<String, String>) todaysActivities.getAdapter().getItem(i);
+                                    try {
+                                        //retrieve info on current activity
+                                        Date startTime = timeFormat2.parse(date.substring(0, 10) + " " + activity.get("activityStartTime"));
+                                        Calendar now = Calendar.getInstance();
+                                        Calendar sTime = Calendar.getInstance();
+                                        sTime.setTime(startTime);
+
+                                        //Check if next activity is less than 10min away
+                                        if ((sTime.getTimeInMillis() - now.getTimeInMillis()) / 60000 < 10 && currentActivityEndTime == "" && (sTime.getTimeInMillis() - now.getTimeInMillis()) / 60000 >= 0) {
+                                            Long diff = sTime.getTimeInMillis() - now.getTimeInMillis();
+                                            diff = diff / 60000;
+                                            labelMinutes.setVisibility(View.VISIBLE);
+                                            labelStatus.setText(Long.toString(diff + 1) + " MIN");
+                                            FrameLayout statusBlock = (FrameLayout) findViewById(R.id.statusBlock);
+                                            statusBlock.setBackgroundResource(status_border_orange);
+                                        }
+
+                                    } catch (ParseException e) {
+                                    }
+
+                                }
+
+                                //Check to see if there is a activity and if it has ended reset the status
+                                if (currentActivityEndTime.equals(currentTime)) {
+                                    //Change status color to green and make text darker for easier visibility
+                                    FrameLayout statusBlock = (FrameLayout) findViewById(R.id.statusBlock);
+                                    statusBlock.setBackgroundResource(status_border_green);
+                                    labelStatus.setTextColor(Color.parseColor("#ff364147"));
+                                    clockTime.setTextColor(Color.parseColor("#ff364147"));
+                                    clockDate.setTextColor(Color.parseColor("#ff364147"));
+
+                                    //Change status to Free
+                                    labelStatus.setText("FREE");
+
+                                    //Set the current activity to empty
+                                    currentActivityEndTime = "";
+
+                                    //Deselect Item from List View and set focus to status block
+                                    todaysActivities.setSelection(-1);
+                                    todaysActivities.clearFocus();
+                                    statusBlock.requestFocusFromTouch();
+                                }
+                            }
+                        });
+                    }
+                } catch (InterruptedException e) {
+                }
+            }
+        };
+
+        statusThread.start();
     }
 
     /**
@@ -103,6 +230,18 @@ public class DefaultView extends Activity {
         activity.put(timeEndKey, endTime);
         return activity;
     }
+
+    /**
+     * Function to compare the activity start times so that the sorter can correctly function
+     *
+     * @param roomEmail - Used to retreive the activities for only this room
+     * @return returns a comparator object
+     */
+    public static Comparator<Map<String, String>> mapComparator = new Comparator<Map<String, String>>() {
+        public int compare(Map<String, String> m1, Map<String, String> m2) {
+            return m1.get("activityStartTime").compareTo(m2.get("activityStartTime"));
+        }
+    };
 
     /**
      * This function is executed when the user clicks the quick book button
@@ -147,8 +286,18 @@ public class DefaultView extends Activity {
                 .setPositiveButton("Enter", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
 
-
-
+                        //Check to see that all required fields have been entered
+                        if (activityDuration.getValue() == 0 && activitySubject.getText().toString().equals("")) {
+                            dialog.cancel();
+                            showNewActivityInputDialog(true, null, 0);
+                        } else if (activityDuration.getValue() == 0 && !activitySubject.getText().toString().equals("")) {
+                            dialog.cancel();
+                            showNewActivityInputDialog(true, activitySubject.getText().toString(), 0);
+                        } else if (activityDuration.getValue() != 0 && activitySubject.getText().toString().equals("")) {
+                            dialog.cancel();
+                            showNewActivityInputDialog(true, null, activityDuration.getValue());
+                        } else {
+                            //Check if the room is available
 
                                 Calendar calendar = Calendar.getInstance();
                                 SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
@@ -172,7 +321,8 @@ public class DefaultView extends Activity {
                                 calendar.add(Calendar.MINUTE, activityDuration.getValue());
                                 String endTime = timeFormat.format(calendar.getTime());
 
-
+                                    //Check that the new activity end time does not clash with the next activity
+                                    if (checkActivityEndTime(startTime, endTime)) {
 
                                             //Let user know activity is being booked
                                             Toast toastAdding= Toast.makeText(getApplicationContext(), "Adding activity: " + activitySubject.getText().toString(), Toast.LENGTH_LONG);
@@ -183,18 +333,24 @@ public class DefaultView extends Activity {
 
                                             currentActivityEndTime = endTime;
 
-
+                                        Collections.sort(activities, mapComparator);
 
                                         todaysActivities = (ListView) findViewById(R.id.lstActivities);
                                         todaysActivities.setAdapter(populateActivities);
 
 
+                                    } else {
+                                        //Send the user a notification saying that the current activity duration clash with the next activity
+                                        Toast toastClash = Toast.makeText(getApplicationContext(), "The current duration will cause the activity to go into the next day!", Toast.LENGTH_SHORT);
+                                        toastClash.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL, 0, 0);
+                                        toastClash.show();
+                                        dialog.cancel();
+                                    }
 
 
 
 
-
-
+                        }
                     }
                 })
                 .setNegativeButton("Cancel",
@@ -235,5 +391,26 @@ public class DefaultView extends Activity {
         //Show the dialog and set its dimensions
         alert.show();
         alert.getWindow().setLayout(800, 900);
+    }
+
+    /**
+     * Check clash times of current activity and the next one
+     *
+     * @return This function return true if no clash occurs
+     */
+    private boolean checkActivityEndTime(String start, String end) {
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
+
+        try {
+            if (timeFormat.parse(start).after(timeFormat.parse(end))) {
+                return true;
+            } else {
+                return true;
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return false;
     }
 }
